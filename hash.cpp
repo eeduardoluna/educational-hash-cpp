d1e57dfd4d78dc9b8bd40b0df41281a2e16507c8
Eduardo Luna <eeduardolluna@gmail.com>
2025-12-23 01:26:44 -0300

    Clarify initial hash constant in simple_hash function
    Added a comment to clarify the initial hash constant.

--- hash.cpp
+++ hash.cpp
@@ -1,9 +1,10 @@
 #include "hash.h"
 uint64_t simple_hash(const std::string& input){
-    uint64_t hash = 14695987034346686737ULL;
+    uint64_t hash = 14695987034346686737ULL; // -partially arbitrary constant
     for(char c : input){
         hash ^= static_cast <uint64_t>(c);
         hash *= 1099511628211ULL; // FNV-1a
     }
     return hash;
 }
+
