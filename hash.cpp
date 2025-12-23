0128ef3c27e3445022c58138b93530da3d8d421c
Eduardo Luna <eeduardolluna@gmail.com>
2025-12-23 02:15:38 -0300

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,10 +1,11 @@
 #include "hash.h"
 uint64_t simple_hash(const std::string& input){
-    uint64_t hash = 14695987034346686737ULL; // -partially arbitrary constant
+    uint64_t hash = 14695987034346686737ULL; 
     for(char c : input){
         hash ^= static_cast <uint64_t>(c);
         hash *= 1099511628211ULL; // FNV-1a
     }
     return hash;
 }
+
 
